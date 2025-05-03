dc8a83a3290fa499419e03b7ec5087132b3d2eaa
Riccardo Omiccioli <omiccioli.riccardo@gmail.com>
2025-05-03 13:02:38 +0200

    configure vue + vite

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const hostname = ref<string>('');
+
+    const fetchHostname = async () => {
+      try {
+        console.log(cockpit);
+        const response = await cockpit.file('/etc/hostname').read();
+        hostname.value = response.trim();
+      } catch (error) {
+        hostname.value = 'Failed to fetch hostname';
+      }
+    };
+
+    return {
+      hostname,
+      fetchHostname
+    };
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <h1>Cockpit Wi-Fi Plugin</h1>
+    <button @click="fetchHostname">Get Hostname</button>
+    <p>{{ hostname }}</p>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+</style>
---
+++ src/cockpit.d.ts
@@ -0,0 +1,1 @@
+declare var cockpit: any;
